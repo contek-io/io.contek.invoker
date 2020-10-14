878a804b25bc457ba7538005f98ff2d04aa7d22b
beijunyi <beijunyi@gmail.com>
2020-10-14 15:45:11 +0800

    Fix bybit GetWalletBalance

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetWalletBalance.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetWalletBalance.java
@@ -1,9 +1,5 @@
 package io.contek.invoker.bybit.api.rest.user;
 
-import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_GET_REQUEST;
-import static io.contek.invoker.bybit.api.rest.user.GetWalletBalance.Response;
-import static io.contek.invoker.commons.api.rest.RestMethod.GET;
-
 import com.google.common.collect.ImmutableList;
 import io.contek.invoker.bybit.api.common._WalletBalance;
 import io.contek.invoker.bybit.api.rest.common.RestResponse;
@@ -12,7 +8,13 @@ import io.contek.invoker.commons.api.actor.ratelimit.RateLimitQuota;
 import io.contek.invoker.commons.api.rest.RestContext;
 import io.contek.invoker.commons.api.rest.RestMethod;
 import io.contek.invoker.commons.api.rest.RestParams;
+
 import javax.annotation.concurrent.NotThreadSafe;
+import java.util.Map;
+
+import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_GET_REQUEST;
+import static io.contek.invoker.bybit.api.rest.user.GetWalletBalance.Response;
+import static io.contek.invoker.commons.api.rest.RestMethod.GET;
 
 @NotThreadSafe
 public final class GetWalletBalance extends UserRestRequest<Response> {
@@ -60,5 +62,5 @@ public final class GetWalletBalance extends UserRestRequest<Response> {
   }
 
   @NotThreadSafe
-  public static final class Response extends RestResponse<_WalletBalance> {}
+  public static final class Response extends RestResponse<Map<String, _WalletBalance>> {}
 }
