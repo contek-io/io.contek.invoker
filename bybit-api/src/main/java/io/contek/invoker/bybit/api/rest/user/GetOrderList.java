82aba574453ccf5b13e603097525b2f72831a469
beijunyi <beijunyi@gmail.com>
2020-10-15 00:34:53 +0800

    Fixing Bybit GetOrderList in 1.2.3

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetOrderList.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetOrderList.java
@@ -1,21 +1,20 @@
 package io.contek.invoker.bybit.api.rest.user;
 
+import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_GET_REQUEST;
+import static io.contek.invoker.bybit.api.rest.user.GetOrderList.Response;
+import static io.contek.invoker.commons.api.rest.RestMethod.GET;
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.collect.ImmutableList;
 import io.contek.invoker.bybit.api.common._Order;
+import io.contek.invoker.bybit.api.rest.common.RestPagedResult;
 import io.contek.invoker.bybit.api.rest.common.RestResponse;
 import io.contek.invoker.commons.api.actor.IActor;
 import io.contek.invoker.commons.api.actor.ratelimit.RateLimitQuota;
 import io.contek.invoker.commons.api.rest.RestContext;
 import io.contek.invoker.commons.api.rest.RestMethod;
 import io.contek.invoker.commons.api.rest.RestParams;
-
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.List;
-
-import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_GET_REQUEST;
-import static io.contek.invoker.bybit.api.rest.user.GetOrderList.Response;
-import static io.contek.invoker.commons.api.rest.RestMethod.GET;
-import static java.util.Objects.requireNonNull;
 
 @NotThreadSafe
 public final class GetOrderList extends UserRestRequest<Response> {
@@ -62,5 +61,5 @@ public final class GetOrderList extends UserRestRequest<Response> {
   }
 
   @NotThreadSafe
-  public static final class Response extends RestResponse<List<_Order>> {}
+  public static final class Response extends RestResponse<RestPagedResult<_Order>> {}
 }
