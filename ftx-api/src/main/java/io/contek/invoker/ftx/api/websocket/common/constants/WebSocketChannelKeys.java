966ff7e559c04b783976f3960fb45af5c7eb328d
beijunyi <beijunyi@gmail.com>
2020-10-30 16:37:00 +0800

    Add ftx TradesChannel

--- ftx-api/src/main/java/io/contek/invoker/ftx/api/websocket/common/constants/WebSocketChannelKeys.java
+++ ftx-api/src/main/java/io/contek/invoker/ftx/api/websocket/common/constants/WebSocketChannelKeys.java
@@ -5,6 +5,8 @@ import javax.annotation.concurrent.Immutable;
 @Immutable
 public final class WebSocketChannelKeys {
 
+  public static final String trades = "trades";
+
   public static final String orderbook = "orderbook";
 
   private WebSocketChannelKeys() {}
---
+++ ftx-api/src/main/java/io/contek/invoker/ftx/api/websocket/market/TradesChannel.java
@@ -0,0 +1,53 @@
+package io.contek.invoker.ftx.api.websocket.market;
+
+import com.google.common.base.Joiner;
+import io.contek.invoker.ftx.api.common._Trade;
+import io.contek.invoker.ftx.api.websocket.WebSocketChannel;
+import io.contek.invoker.ftx.api.websocket.common.WebSocketChannelMessage;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.ArrayList;
+
+import static io.contek.invoker.ftx.api.websocket.common.constants.WebSocketChannelKeys.trades;
+
+@ThreadSafe
+public final class TradesChannel extends WebSocketChannel<TradesChannel.Message> {
+
+  private final String market;
+
+  TradesChannel(String market) {
+    this.market = market;
+  }
+
+  @Override
+  protected String getChannel() {
+    return trades;
+  }
+
+  @Override
+  protected String getMarket() {
+    return market;
+  }
+
+  @Override
+  protected String getDisplayName() {
+    return Joiner.on(':').join(trades, market);
+  }
+
+  @Override
+  protected Class<TradesChannel.Message> getMessageType() {
+    return TradesChannel.Message.class;
+  }
+
+  @Override
+  protected boolean accepts(TradesChannel.Message message) {
+    return market.equals(message.market);
+  }
+
+  @NotThreadSafe
+  public static final class Data extends ArrayList<_Trade> {}
+
+  @NotThreadSafe
+  public static final class Message extends WebSocketChannelMessage<TradesChannel.Data> {}
+}
