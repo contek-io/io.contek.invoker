cb5658444fe2fe2511febd128c0e4cd4ddaa7fad
beijunyi <beijunyi@gmail.com>
2020-10-31 17:38:35 +0800

    Add FTX enum keys

---
+++ ftx-api/src/main/java/io/contek/invoker/ftx/api/common/constants/MarketTypeKeys.java
@@ -0,0 +1,13 @@
+package io.contek.invoker.ftx.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class MarketTypeKeys {
+
+  public static final String _future = "future";
+
+  public static final String _spot = "spot";
+
+  private MarketTypeKeys() {}
+}
---
+++ ftx-api/src/main/java/io/contek/invoker/ftx/api/common/constants/OrderTypeKeys.java
@@ -0,0 +1,13 @@
+package io.contek.invoker.ftx.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class OrderTypeKeys {
+
+  public static final String _limit = "limit";
+
+  public static final String _market = "market";
+
+  private OrderTypeKeys() {}
+}
