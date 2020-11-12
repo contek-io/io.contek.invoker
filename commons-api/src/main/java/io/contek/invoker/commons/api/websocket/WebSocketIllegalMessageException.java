57219a0630ebc71fcd238b619b9889b26ba9db75
beijunyi <beijunyi@gmail.com>
2020-11-12 11:08:40 +0800

    Release 2.2.0

--- commons-api/src/main/java/io/contek/invoker/commons/api/websocket/WebSocketIllegalMessageException.java
+++ commons-api/src/main/java/io/contek/invoker/commons/api/websocket/WebSocketIllegalMessageException.java
@@ -3,4 +3,19 @@ package io.contek.invoker.commons.api.websocket;
 import javax.annotation.concurrent.Immutable;
 
 @Immutable
-public final class WebSocketIllegalMessageException extends RuntimeException {}
+public final class WebSocketIllegalMessageException extends RuntimeException {
+
+  public WebSocketIllegalMessageException() {}
+
+  public WebSocketIllegalMessageException(String message) {
+    super(message);
+  }
+
+  public WebSocketIllegalMessageException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public WebSocketIllegalMessageException(Throwable cause) {
+    super(cause);
+  }
+}
