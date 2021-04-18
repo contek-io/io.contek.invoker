c5c5fc2f212d3158a0d63a654f7ad04358a80964
beijunyi <beijunyi@gmail.com>
2021-04-18 17:15:30 +0800

    2.8.7 HBDM int keys

---
+++ hbdmlinear-api/src/main/java/io/contek/invoker/hbdmlinear/api/common/constants/OrderTypeKeys.java
@@ -0,0 +1,14 @@
+package io.contek.invoker.hbdmlinear.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class OrderTypeKeys {
+
+  public static final int _1_quotation = 1;
+  public static final int _2_cancelled_order = 2;
+  public static final int _3_forced_liquidation = 3;
+  public static final int _4_delivery_order = 4;
+
+  private OrderTypeKeys() {}
+}
