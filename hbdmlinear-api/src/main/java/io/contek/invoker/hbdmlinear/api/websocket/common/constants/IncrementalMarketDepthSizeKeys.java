5b554941c895fa584868885431e3e2cabdded465
beijunyi <beijunyi@gmail.com>
2021-05-02 14:57:16 +0800

    2.8.15-SNAPSHOT IncrementalMarketDepthSizeKeys

---
+++ hbdmlinear-api/src/main/java/io/contek/invoker/hbdmlinear/api/websocket/common/constants/IncrementalMarketDepthSizeKeys.java
@@ -0,0 +1,12 @@
+package io.contek.invoker.hbdmlinear.api.websocket.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class IncrementalMarketDepthSizeKeys {
+
+  public static final int _20_unmerged_data = 20;
+  public static final int _150_unmerged_data = 150;
+
+  private IncrementalMarketDepthSizeKeys() {}
+}
