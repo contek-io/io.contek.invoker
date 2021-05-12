0d253372373aa59f09ed1f61ac5ffa298bbafe95
beijunyi <beijunyi@gmail.com>
2021-05-12 18:47:41 +0800

    2.10.3-SNAPSHOT minor hbdm inverse fix

--- hbdminverse-api/src/main/java/io/contek/invoker/hbdminverse/api/websocket/market/MarketWebSocketChannelMessage.java
+++ hbdminverse-api/src/main/java/io/contek/invoker/hbdminverse/api/websocket/market/MarketWebSocketChannelMessage.java
@@ -1,11 +1,11 @@
 package io.contek.invoker.hbdminverse.api.websocket.market;
 
-import io.contek.invoker.commons.websocket.AnyWebSocketMessage;
+import io.contek.invoker.hbdminverse.api.websocket.common.WebSocketInboundMessage;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
 @NotThreadSafe
-public abstract class MarketWebSocketChannelMessage extends AnyWebSocketMessage {
+public abstract class MarketWebSocketChannelMessage extends WebSocketInboundMessage {
 
   public String ch;
 }
