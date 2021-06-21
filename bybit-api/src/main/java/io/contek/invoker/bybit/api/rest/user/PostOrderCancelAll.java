ed102270d1de74fffaae607e689912b37f5b0336
Cypher01 <cypher@cypher.com>
2021-06-21 23:48:10 +0200

    Bybit: Add PostOrderCancelAll

---
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/PostOrderCancelAll.java
@@ -0,0 +1,66 @@
+package io.contek.invoker.bybit.api.rest.user;
+
+import com.google.common.collect.ImmutableList;
+import io.contek.invoker.bybit.api.common._OrderCancelAll;
+import io.contek.invoker.bybit.api.rest.common.RestResponse;
+import io.contek.invoker.commons.actor.IActor;
+import io.contek.invoker.commons.actor.ratelimit.RateLimitQuota;
+import io.contek.invoker.commons.rest.RestContext;
+import io.contek.invoker.commons.rest.RestMethod;
+import io.contek.invoker.commons.rest.RestParams;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.util.List;
+
+import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_PRIVATE_ORDER_WRITE_REQUEST;
+import static io.contek.invoker.bybit.api.rest.user.PostOrderCancelAll.Response;
+import static io.contek.invoker.commons.rest.RestMethod.POST;
+import static java.util.Objects.requireNonNull;
+
+@NotThreadSafe
+public final class PostOrderCancelAll extends UserRestRequest<Response> {
+
+  private String symbol;
+
+  PostOrderCancelAll(IActor actor, RestContext context) {
+    super(actor, context);
+  }
+
+  public PostOrderCancelAll setSymbol(String symbol) {
+    this.symbol = symbol;
+    return this;
+  }
+
+  @Override
+  protected RestMethod getMethod() {
+    return POST;
+  }
+
+  @Override
+  protected String getEndpointPath() {
+    return "/v2/private/order/cancelAll";
+  }
+
+  @Override
+  protected RestParams getParams() {
+    RestParams.Builder builder = RestParams.newBuilder();
+
+    requireNonNull(symbol);
+    builder.add("symbol", symbol);
+
+    return builder.build();
+  }
+
+  @Override
+  protected ImmutableList<RateLimitQuota> getRequiredQuotas() {
+    return ONE_REST_PRIVATE_ORDER_WRITE_REQUEST;
+  }
+
+  @Override
+  protected Class<Response> getResponseType() {
+    return Response.class;
+  }
+
+  @NotThreadSafe
+  public static final class Response extends RestResponse<List<_OrderCancelAll>> {}
+}
