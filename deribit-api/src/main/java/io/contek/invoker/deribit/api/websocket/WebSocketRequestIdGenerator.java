b2bae4806ab803b8ee6961c2b12b50a01ae9b32b
bjy <bjy@contek.io>
2021-10-26 18:39:12 +0800

    Prepare to release 2.15.1-SNAPSHOT

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/WebSocketRequestIdGenerator.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/WebSocketRequestIdGenerator.java
@@ -1,14 +1,24 @@
 package io.contek.invoker.deribit.api.websocket;
 
+import io.contek.invoker.deribit.api.websocket.common.WebSocketResponse;
+
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
-public class WebSocketRequestIdGenerator {
+public final class WebSocketRequestIdGenerator {
+
+  private final WebSocketMessageParser parser;
 
   private final AtomicInteger count = new AtomicInteger(0);
 
-  public int getNextRequestId() {
-    return count.incrementAndGet();
+  public WebSocketRequestIdGenerator(WebSocketMessageParser parser) {
+    this.parser = parser;
+  }
+
+  public int getNextRequestId(Class<? extends WebSocketResponse<?>> type) {
+    int id = count.incrementAndGet();
+    parser.register(id, type);
+    return id;
   }
 }
--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/common/WebSocketResponse.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/common/WebSocketResponse.java
@@ -3,13 +3,12 @@ package io.contek.invoker.deribit.api.websocket.common;
 import io.contek.invoker.deribit.api.common._Error;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.List;
 
 @NotThreadSafe
-public final class WebSocketResponse extends WebSocketInboundMessage {
+public abstract class WebSocketResponse<R> extends WebSocketInboundMessage {
 
   public Integer id;
   public String jsonrpc;
-  public List<String> result;
+  public R result;
   public _Error error;
 }
