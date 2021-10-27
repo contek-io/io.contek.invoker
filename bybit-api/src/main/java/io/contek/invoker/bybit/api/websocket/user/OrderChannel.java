7029707a38e40b3a2ce4731d414a2fa65505d278
bjy <bjy@contek.io>
2021-10-27 18:51:20 +0800

    2.15.18-SNAPSHOT

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/websocket/user/OrderChannel.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/websocket/user/OrderChannel.java
@@ -1,6 +1,5 @@
 package io.contek.invoker.bybit.api.websocket.user;
 
-import io.contek.invoker.bybit.api.common._Order;
 import io.contek.invoker.bybit.api.websocket.WebSocketChannel;
 import io.contek.invoker.bybit.api.websocket.WebSocketChannelId;
 import io.contek.invoker.bybit.api.websocket.common.WebSocketTopicMessage;
@@ -35,6 +34,33 @@ public final class OrderChannel extends WebSocketChannel<OrderChannel.Id, OrderC
   @NotThreadSafe
   public static final class Message extends WebSocketTopicMessage {
 
-    public List<_Order> data;
+    public List<Data> data;
+  }
+
+  @NotThreadSafe
+  public static final class Data {
+
+    public String order_id;
+    public String order_link_id;
+    public String symbol;
+    public String side;
+    public String order_type;
+    public String price;
+    public int qty;
+    public String time_in_force;
+    public String create_type;
+    public String cancel_type;
+    public String order_status;
+    public int leaves_qty;
+    public int cum_exec_qty;
+    public String cum_exec_value;
+    public String cum_exec_fee;
+    public String timestamp;
+    public String take_profit;
+    public String stop_loss;
+    public String trailing_stop;
+    public String last_exec_price;
+    public boolean reduce_only;
+    public boolean close_on_trigger;
   }
 }
