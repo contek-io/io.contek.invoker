fee8e7080dfdb1f5963d4534f67fbcb79829d67a
bjy <bjy@contek.io>
2021-10-27 15:51:14 +0800

    2.15.16-SNAPSHOT

--- ftx-api/src/main/java/io/contek/invoker/ftx/api/websocket/WebSocketChannelId.java
+++ ftx-api/src/main/java/io/contek/invoker/ftx/api/websocket/WebSocketChannelId.java
@@ -5,39 +5,26 @@ import io.contek.invoker.ftx.api.websocket.common.WebSocketChannelMessage;
 
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
-import java.util.Objects;
 
 @Immutable
 public abstract class WebSocketChannelId<Message extends WebSocketChannelMessage<?>>
     extends BaseWebSocketChannelId<Message> {
 
   private final String channel;
-  private final String market;
 
-  protected WebSocketChannelId(String channel, @Nullable String market) {
-    super(combine(channel, market));
+  protected WebSocketChannelId(String channel, @Nullable String suffix) {
+    super(combine(channel, suffix));
     this.channel = channel;
-    this.market = market;
   }
 
   public final String getChannel() {
     return channel;
   }
 
-  @Nullable
-  public final String getMarket() {
-    return market;
-  }
-
-  @Override
-  public final boolean accepts(Message message) {
-    return Objects.equals(channel, message.channel) && Objects.equals(market, message.market);
-  }
-
-  private static String combine(String channel, @Nullable String market) {
-    if (market == null) {
+  private static String combine(String channel, @Nullable String suffix) {
+    if (suffix == null) {
       return channel;
     }
-    return String.join(".", channel, market);
+    return String.join(".", channel, suffix);
   }
 }
