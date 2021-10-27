06552edb5b0cdfe659afba2d41a98ab48ee2b544
bjy <bjy@contek.io>
2021-10-27 13:47:35 +0800

    2.15.8-SNAPSHOT

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/websocket/WebSocketAuthenticator.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/websocket/WebSocketAuthenticator.java
@@ -7,6 +7,7 @@ import io.contek.invoker.commons.websocket.AnyWebSocketMessage;
 import io.contek.invoker.commons.websocket.IWebSocketAuthenticator;
 import io.contek.invoker.commons.websocket.WebSocketSession;
 import io.contek.invoker.security.ICredential;
+import org.slf4j.Logger;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.time.Clock;
@@ -14,11 +15,13 @@ import java.time.Duration;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static io.contek.invoker.bybit.api.websocket.common.constants.WebSocketOperationKeys._auth;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ThreadSafe
 final class WebSocketAuthenticator implements IWebSocketAuthenticator {
 
   private static final Duration EXPIRE_DELAY = Duration.ofSeconds(30);
+  private static final Logger log = getLogger(WebSocketAuthenticator.class);
 
   private final ICredential credential;
   private final Clock clock;
@@ -46,6 +49,7 @@ final class WebSocketAuthenticator implements IWebSocketAuthenticator {
     request.op = _auth;
     request.args = ImmutableList.of(key, expires, signature);
 
+    log.info("Requesting authentication for {}.", credential.getApiKeyId());
     session.send(request);
   }
 
@@ -72,7 +76,9 @@ final class WebSocketAuthenticator implements IWebSocketAuthenticator {
     if (!confirmation.success) {
       throw new IllegalStateException();
     }
+
     authenticated.set(true);
+    log.info("Authentication for {} completed.", credential.getApiKeyId());
   }
 
   @Override
