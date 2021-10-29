87d12264ab0a76ec3d7f510a42b1cdef1ed1a9bc
bjy <bjy@contek.io>
2021-10-29 15:57:04 +0800

    2.15.21-SNAPSHOT

--- commons/src/main/java/io/contek/invoker/commons/actor/http/HttpInterruptedException.java
+++ commons/src/main/java/io/contek/invoker/commons/actor/http/HttpInterruptedException.java
@@ -1,11 +1,16 @@
 package io.contek.invoker.commons.actor.http;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.io.InterruptedIOException;
 
 @NotThreadSafe
 public final class HttpInterruptedException extends AnyHttpException {
 
   public HttpInterruptedException(InterruptedException cause) {
+    super(cause);
+  }
+
+  public HttpInterruptedException(InterruptedIOException cause) {
     super(cause);
   }
 }
