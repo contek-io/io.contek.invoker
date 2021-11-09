4cf52cbd68416f14e58ccc8dab34483c56b0bbee
bjy <bjy@contek.io>
2021-11-09 13:25:37 +0800

    2.15.23-SNAPSHOT

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/common/WebSocketUnsubscribeConfirmation.java
+++
@@ -1,7 +0,0 @@
-package io.contek.invoker.deribit.api.websocket.common;
-
-import javax.annotation.concurrent.NotThreadSafe;
-import java.util.List;
-
-@NotThreadSafe
-public final class WebSocketUnsubscribeConfirmation extends WebSocketResponse<List<String>> {}
