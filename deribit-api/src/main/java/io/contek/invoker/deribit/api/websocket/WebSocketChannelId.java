e2f0b0f4026a6985e1e163ac478f038ea6d534a3
bjy <bjy@contek.io>
2021-11-18 18:44:19 +0800

    2.15.28 deribit

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/WebSocketChannelId.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/WebSocketChannelId.java
@@ -1,12 +1,12 @@
 package io.contek.invoker.deribit.api.websocket;
 
 import io.contek.invoker.commons.websocket.BaseWebSocketChannelId;
-import io.contek.invoker.deribit.api.websocket.common.WebSocketChannelMessage;
+import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMessage;
 
 import javax.annotation.concurrent.Immutable;
 
 @Immutable
-public abstract class WebSocketChannelId<Message extends WebSocketChannelMessage<?>>
+public abstract class WebSocketChannelId<Message extends WebSocketSingleChannelMessage<?>>
     extends BaseWebSocketChannelId<Message> {
 
   protected WebSocketChannelId(String channel) {
