27223d9a1bf2284adbc2e07b5fdb002d935ee2e0
bjy <bjy@contek.io>
2021-11-18 18:44:19 +0800

    2.15.28 deribit

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/market/MarketWebSocketChannel.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/market/MarketWebSocketChannel.java
@@ -3,13 +3,13 @@ package io.contek.invoker.deribit.api.websocket.market;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannel;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannelId;
 import io.contek.invoker.deribit.api.websocket.WebSocketRequestIdGenerator;
-import io.contek.invoker.deribit.api.websocket.common.WebSocketChannelMessage;
+import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMessage;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 @ThreadSafe
 public abstract class MarketWebSocketChannel<
-        Id extends WebSocketChannelId<Message>, Message extends WebSocketChannelMessage<?>>
+        Id extends WebSocketChannelId<Message>, Message extends WebSocketSingleChannelMessage<?>>
     extends WebSocketChannel<Id, Message> {
 
   MarketWebSocketChannel(Id id, WebSocketRequestIdGenerator requestIdGenerator) {
