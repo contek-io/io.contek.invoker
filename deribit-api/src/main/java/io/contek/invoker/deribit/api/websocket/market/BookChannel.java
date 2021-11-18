ebbae3bd0ab9f7ff70d4df835fa428b23eead65d
bjy <bjy@contek.io>
2021-11-18 18:44:19 +0800

    2.15.28 deribit

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/market/BookChannel.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/market/BookChannel.java
@@ -3,7 +3,7 @@ package io.contek.invoker.deribit.api.websocket.market;
 import io.contek.invoker.deribit.api.common._OrderBookLevel;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannelId;
 import io.contek.invoker.deribit.api.websocket.WebSocketRequestIdGenerator;
-import io.contek.invoker.deribit.api.websocket.common.WebSocketChannelMessage;
+import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMessage;
 
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.NotThreadSafe;
@@ -38,7 +38,7 @@ public final class BookChannel extends MarketWebSocketChannel<BookChannel.Id, Bo
   }
 
   @NotThreadSafe
-  public static final class Message extends WebSocketChannelMessage<Data> {}
+  public static final class Message extends WebSocketSingleChannelMessage<Data> {}
 
   @NotThreadSafe
   public static final class Data {
