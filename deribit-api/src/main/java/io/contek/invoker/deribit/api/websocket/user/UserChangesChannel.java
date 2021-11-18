61f7e6c7567bb85795fd1b9233cf93c556fdbc32
bjy <bjy@contek.io>
2021-11-18 18:44:19 +0800

    2.15.28 deribit

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/user/UserChangesChannel.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/user/UserChangesChannel.java
@@ -1,6 +1,8 @@
 package io.contek.invoker.deribit.api.websocket.user;
 
 import io.contek.invoker.deribit.api.common._Order;
+import io.contek.invoker.deribit.api.common._Position;
+import io.contek.invoker.deribit.api.common._Trade;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannelId;
 import io.contek.invoker.deribit.api.websocket.WebSocketRequestIdGenerator;
 import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMessage;
@@ -8,31 +10,32 @@ import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMess
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.annotation.concurrent.ThreadSafe;
+import java.util.List;
 
 import static java.lang.String.format;
 
 @ThreadSafe
-public final class UserOrdersChannel
-    extends UserWebSocketChannel<UserOrdersChannel.Id, UserOrdersChannel.Message> {
+public final class UserChangesChannel
+    extends UserWebSocketChannel<UserChangesChannel.Id, UserChangesChannel.Message> {
 
-  UserOrdersChannel(Id id, WebSocketRequestIdGenerator requestIdGenerator) {
+  UserChangesChannel(Id id, WebSocketRequestIdGenerator requestIdGenerator) {
     super(id, requestIdGenerator);
   }
 
   @Override
-  public Class<UserOrdersChannel.Message> getMessageType() {
-    return UserOrdersChannel.Message.class;
+  public Class<UserChangesChannel.Message> getMessageType() {
+    return UserChangesChannel.Message.class;
   }
 
   @Immutable
-  public static final class Id extends WebSocketChannelId<UserOrdersChannel.Message> {
+  public static final class Id extends WebSocketChannelId<UserChangesChannel.Message> {
 
     private Id(String value) {
       super(value);
     }
 
     public static Id of(String instrumentName, String interval) {
-      return new Id(format("user.orders.%s.%s", instrumentName, interval));
+      return new Id(format("user.changes.%s.%s", instrumentName, interval));
     }
   }
 
@@ -40,5 +43,11 @@ public final class UserOrdersChannel
   public static final class Message extends WebSocketSingleChannelMessage<Data> {}
 
   @NotThreadSafe
-  public static final class Data extends _Order {}
+  public static final class Data {
+
+    public String instrument_name;
+    public List<_Order> orders;
+    public List<_Position> positions;
+    public List<_Trade> trades;
+  }
 }
