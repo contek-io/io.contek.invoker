5d330feea8169e6d9e8d0ad5d48a382246f8cf45
bjy <bjy@contek.io>
2021-11-18 18:44:19 +0800

    2.15.28 deribit

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/user/UserWebSocketChannel.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/user/UserWebSocketChannel.java
@@ -3,13 +3,13 @@ package io.contek.invoker.deribit.api.websocket.user;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannel;
 import io.contek.invoker.deribit.api.websocket.WebSocketChannelId;
 import io.contek.invoker.deribit.api.websocket.WebSocketRequestIdGenerator;
-import io.contek.invoker.deribit.api.websocket.common.WebSocketChannelMessage;
+import io.contek.invoker.deribit.api.websocket.common.WebSocketSingleChannelMessage;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 @ThreadSafe
 public abstract class UserWebSocketChannel<
-        Id extends WebSocketChannelId<Message>, Message extends WebSocketChannelMessage<?>>
+        Id extends WebSocketChannelId<Message>, Message extends WebSocketSingleChannelMessage<?>>
     extends WebSocketChannel<Id, Message> {
 
   UserWebSocketChannel(Id id, WebSocketRequestIdGenerator requestIdGenerator) {
