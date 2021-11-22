a8fea5dee5e330b7e95bb3f1892f1f55a8b54605
bjy <bjy@contek.io>
2021-11-22 18:24:23 +0800

    2.15.31 GetExecutionTradeHistory

--- bitmex-api/src/main/java/io/contek/invoker/bitmex/api/rest/user/UserRestApi.java
+++ bitmex-api/src/main/java/io/contek/invoker/bitmex/api/rest/user/UserRestApi.java
@@ -24,6 +24,10 @@ public final class UserRestApi {
     return new DeleteOrderAll(actor, context);
   }
 
+  public GetExecutionTradeHistory getExecutionTradeHistory() {
+    return new GetExecutionTradeHistory(actor, context);
+  }
+
   public GetOrder getOrder() {
     return new GetOrder(actor, context);
   }
