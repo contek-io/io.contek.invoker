e7b84ce2306f44bf43076107629169a9598e49d7
bjy <bjy@contek.io>
2021-11-22 18:24:23 +0800

    2.15.31 GetExecutionTradeHistory

---
+++ bitmex-api/src/main/java/io/contek/invoker/bitmex/api/common/_Execution.java
@@ -0,0 +1,55 @@
+package io.contek.invoker.bitmex.api.common;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+public class _Execution {
+
+  public String execID;
+  public String orderID;
+  public String clOrdID;
+  public String clOrdLinkID;
+  public Long account;
+  public String symbol;
+  public String side;
+  public Double lastQty;
+  public Double lastPx;
+  public Double underlyingLastPx;
+  public String lastMkt;
+  public String lastLiquidityInd;
+  public Double simpleOrderQty;
+  public Double orderQty;
+  public Double price;
+  public Double displayQty;
+  public Double stopPx;
+  public Double pegOffsetValue;
+  public String pegPriceType;
+  public String currency;
+  public String settlCurrency;
+  public String execType;
+  public String ordType;
+  public String timeInForce;
+  public String execInst;
+  public String contingencyType;
+  public String exDestination;
+  public String ordStatus;
+  public String triggered;
+  public Boolean workingIndicator;
+  public String ordRejReason;
+  public Double simpleLeavesQty;
+  public Double leavesQty;
+  public Double simpleCumQty;
+  public Double cumQty;
+  public Double avgPx;
+  public Double commission;
+  public String tradePublishIndicator;
+  public String multiLegReportingType;
+  public String text;
+  public String trdMatchID;
+  public Double execCost;
+  public Double execComm;
+  public Double homeNotional;
+  public Double foreignNotional;
+  public String transactTime;
+  public String timestamp;
+}
