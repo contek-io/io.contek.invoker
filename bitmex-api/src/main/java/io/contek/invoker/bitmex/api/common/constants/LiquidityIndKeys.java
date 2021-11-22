63b11a0454dca6606ff67c73a3adcb45d18d5b8a
bjy <bjy@contek.io>
2021-11-22 18:55:14 +0800

    2.15.32 LiquidityIndKeys

---
+++ bitmex-api/src/main/java/io/contek/invoker/bitmex/api/common/constants/LiquidityIndKeys.java
@@ -0,0 +1,13 @@
+package io.contek.invoker.bitmex.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class LiquidityIndKeys {
+
+  public static final String _AddedLiquidity = "AddedLiquidity";
+
+  public static final String _RemovedLiquidity = "RemovedLiquidity";
+
+  private LiquidityIndKeys() {}
+}
