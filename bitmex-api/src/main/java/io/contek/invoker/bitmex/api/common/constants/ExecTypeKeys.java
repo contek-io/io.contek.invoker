4ecef5746aa9e0e31b3650b63cae4e15b19b3d57
bjy <bjy@contek.io>
2021-11-23 11:13:26 +0800

    2.15.35

---
+++ bitmex-api/src/main/java/io/contek/invoker/bitmex/api/common/constants/ExecTypeKeys.java
@@ -0,0 +1,15 @@
+package io.contek.invoker.bitmex.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class ExecTypeKeys {
+
+  public static final String _Settlement = "Settlement";
+
+  public static final String _Trade = "Trade";
+
+  public static final String _Funding = "Funding";
+
+  private ExecTypeKeys() {}
+}
