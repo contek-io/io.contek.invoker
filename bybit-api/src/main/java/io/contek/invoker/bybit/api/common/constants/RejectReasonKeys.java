ba30b5b07b3a4b723e8c03d95a670b1ad6f31bd7
bjy <bjy@contek.io>
2021-12-08 10:34:52 +0800

    2.15.37

---
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/common/constants/RejectReasonKeys.java
@@ -0,0 +1,15 @@
+package io.contek.invoker.bybit.api.common.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class RejectReasonKeys {
+
+  public static final String _EC_PostOnlyWillTakeLiquidity = "EC_PostOnlyWillTakeLiquidity";
+
+  public static final String _EC_NoImmediateQtyToFill = "EC_NoImmediateQtyToFill";
+
+  public static final String _EC_NoError = "EC_NoError";
+
+  private RejectReasonKeys() {}
+}
