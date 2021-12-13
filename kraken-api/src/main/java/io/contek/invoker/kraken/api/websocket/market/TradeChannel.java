f0495ad6c4c7f786344fc0639217135125ed45db
bjy <bjy@contek.io>
2021-12-13 16:27:07 +0800

    2.15.38

--- kraken-api/src/main/java/io/contek/invoker/kraken/api/websocket/market/TradeChannel.java
+++ kraken-api/src/main/java/io/contek/invoker/kraken/api/websocket/market/TradeChannel.java
@@ -6,14 +6,13 @@ import io.contek.invoker.kraken.api.websocket.WebSocketChannelId;
 import io.contek.invoker.kraken.api.websocket.WebSocketRequestIdGenerator;
 import io.contek.invoker.kraken.api.websocket.common.Subscription;
 import io.contek.invoker.kraken.api.websocket.common.WebSocketChannelDataMessage;
-import io.contek.invoker.kraken.api.websocket.common.constants.WebSocketChannelKeys;
 
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.List;
 
-import static io.contek.invoker.kraken.api.websocket.common.constants.WebSocketChannelKeys._book;
+import static io.contek.invoker.kraken.api.websocket.common.constants.WebSocketChannelKeys._trade;
 
 @ThreadSafe
 public final class TradeChannel extends WebSocketChannel<TradeChannel.Id, TradeChannel.Message> {
@@ -30,7 +29,7 @@ public final class TradeChannel extends WebSocketChannel<TradeChannel.Id, TradeC
   @Override
   protected Subscription getSubscription() {
     Subscription subscription = new Subscription();
-    subscription.name = WebSocketChannelKeys._trade;
+    subscription.name = _trade;
     return subscription;
   }
 
@@ -38,7 +37,7 @@ public final class TradeChannel extends WebSocketChannel<TradeChannel.Id, TradeC
   public static final class Id extends WebSocketChannelId<TradeChannel.Message> {
 
     private Id(String pair) {
-      super(_book, pair);
+      super(_trade, pair);
     }
 
     public static Id of(String pair) {
