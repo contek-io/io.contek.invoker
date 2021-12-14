7d0c2761d89e7e022d0b19244a89b8d05030f83a
bjy <bjy@contek.io>
2021-12-14 11:57:18 +0800

    binance trade list

---
+++ binancefutures-api/src/main/java/io/contek/invoker/binancefutures/api/common/_UserTrade.java
@@ -0,0 +1,22 @@
+package io.contek.invoker.binancefutures.api.common;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+public class _UserTrade {
+
+  public Boolean buyer;
+  public String commission;
+  public String commissionAsset;
+  public Long id;
+  public Boolean maker;
+  public Long orderId;
+  public String price;
+  public String qty;
+  public String quoteQty;
+  public String realizedPnl;
+  public String side;
+  public String positionSide;
+  public String symbol;
+  public Long time;
+}
