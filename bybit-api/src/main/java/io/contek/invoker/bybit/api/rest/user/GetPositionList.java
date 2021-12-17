6075f51bef28f5f47f11514cac6b65e29467ed6d
bjy <bjy@contek.io>
2021-12-17 18:34:35 +0800

    2.16.3

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetPositionList.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/GetPositionList.java
@@ -10,7 +10,6 @@ import io.contek.invoker.commons.rest.RestMethod;
 import io.contek.invoker.commons.rest.RestParams;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.List;
 
 import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_PRIVATE_POSITION_READ_REQUEST;
 import static io.contek.invoker.bybit.api.rest.user.GetPositionList.Response;
@@ -62,12 +61,5 @@ public final class GetPositionList extends UserRestRequest<Response> {
   }
 
   @NotThreadSafe
-  public static final class Response extends RestResponse<List<Result>> {}
-
-  @NotThreadSafe
-  public static final class Result {
-
-    public boolean is_valid;
-    public _Position data;
-  }
+  public static final class Response extends RestResponse<_Position> {}
 }
