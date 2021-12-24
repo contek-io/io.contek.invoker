69cf1aab45d1648a5ecd04bc26bb6cf0506d1733
bjy <bjy@contek.io>
2021-12-24 15:18:48 +0800

    2.16.11-SNAPSHOT

--- bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/PostPositionSwitchIsolated.java
+++ bybit-api/src/main/java/io/contek/invoker/bybit/api/rest/user/PostPositionSwitchIsolated.java
@@ -1,7 +1,7 @@
-package io.contek.invoker.bybitlinear.api.rest.user;
+package io.contek.invoker.bybit.api.rest.user;
 
 import com.google.common.collect.ImmutableList;
-import io.contek.invoker.bybitlinear.api.rest.common.RestResponse;
+import io.contek.invoker.bybit.api.rest.common.RestResponse;
 import io.contek.invoker.commons.actor.IActor;
 import io.contek.invoker.commons.actor.ratelimit.TypedPermitRequest;
 import io.contek.invoker.commons.rest.RestContext;
@@ -10,33 +10,39 @@ import io.contek.invoker.commons.rest.RestParams;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
-import static io.contek.invoker.bybitlinear.api.ApiFactory.RateLimits.ONE_REST_PRIVATE_POSITION_WRITE_REQUEST;
-import static io.contek.invoker.bybitlinear.api.rest.user.PostLeverageSave.Response;
+import static io.contek.invoker.bybit.api.ApiFactory.RateLimits.ONE_REST_PRIVATE_POSITION_WRITE_REQUEST;
+import static io.contek.invoker.bybit.api.rest.user.PostPositionSwitchIsolated.Response;
 import static io.contek.invoker.commons.rest.RestMethod.POST;
 import static java.util.Objects.requireNonNull;
 
 @NotThreadSafe
-public final class PostLeverageSave extends UserRestRequest<Response> {
+public final class PostPositionSwitchIsolated extends UserRestRequest<Response> {
 
   private String symbol;
+  private Boolean is_isolated;
   private Double buy_leverage;
   private Double sell_leverage;
 
-  PostLeverageSave(IActor actor, RestContext context) {
+  PostPositionSwitchIsolated(IActor actor, RestContext context) {
     super(actor, context);
   }
 
-  public PostLeverageSave setSymbol(String symbol) {
+  public PostPositionSwitchIsolated setSymbol(String symbol) {
     this.symbol = symbol;
     return this;
   }
 
-  public PostLeverageSave setBuyLeverage(Double buy_leverage) {
+  public PostPositionSwitchIsolated setIsIsolated(Boolean is_isolated) {
+    this.is_isolated = is_isolated;
+    return this;
+  }
+
+  public PostPositionSwitchIsolated setBuyLeverage(Double buy_leverage) {
     this.buy_leverage = buy_leverage;
     return this;
   }
 
-  public PostLeverageSave setSellLeverage(Double sell_leverage) {
+  public PostPositionSwitchIsolated setSellLeverage(Double sell_leverage) {
     this.sell_leverage = sell_leverage;
     return this;
   }
@@ -48,7 +54,7 @@ public final class PostLeverageSave extends UserRestRequest<Response> {
 
   @Override
   protected String getEndpointPath() {
-    return "/private/linear/position/set-leverage";
+    return "/v2/private/position/switch-isolated";
   }
 
   @Override
@@ -57,6 +63,9 @@ public final class PostLeverageSave extends UserRestRequest<Response> {
 
     requireNonNull(symbol);
     builder.add("symbol", symbol);
+
+    requireNonNull(is_isolated);
+    builder.add("is_isolated", is_isolated);
 
     requireNonNull(buy_leverage);
     builder.add("buy_leverage", buy_leverage);
