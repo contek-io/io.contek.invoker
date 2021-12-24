6fa6a22accb0afcf08dd4e4954b4ccb3b745bee1
bjy <bjy@contek.io>
2021-12-24 15:18:48 +0800

    2.16.11-SNAPSHOT

--- bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/user/UserRestApi.java
+++ bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/user/UserRestApi.java
@@ -44,8 +44,8 @@ public final class UserRestApi {
     return new GetWalletBalance(actor, context);
   }
 
-  public PostLeverageSave postLeverageSave() {
-    return new PostLeverageSave(actor, context);
+  public PostPositionSwitchIsolated postPositionSwitchIsolated() {
+    return new PostPositionSwitchIsolated(actor, context);
   }
 
   public PostOrderCancel postOrderCancel() {
