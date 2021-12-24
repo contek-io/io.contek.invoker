58104e8e676db4d297c8ca245f4beddb2f037ce1
bjy <bjy@contek.io>
2021-12-24 11:55:23 +0800

    2.16.8-SNAPSHOT

--- bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/common/_Order.java
+++ bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/common/_Order.java
@@ -5,23 +5,22 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public class _Order {
 
-  public long user_id;
   public String order_id;
+  public Long user_id;
   public String symbol;
   public String side;
   public String order_type;
-  public double price;
-  public int qty;
+  public Double price;
+  public Double qty;
   public String time_in_force;
   public String order_status;
-  public String last_exec_time;
   public String last_exec_price;
-  public int leaves_qty;
-  public int cum_exec_qty;
-  public double cum_exec_value;
-  public double cum_exec_fee;
-  public String reject_reason;
+  public Double cum_exec_qty;
+  public Double cum_exec_value;
+  public Double cum_exec_fee;
   public String order_link_id;
+  public Boolean reduce_only;
+  public Boolean close_on_trigger;
   public String created_at;
   public String updated_at;
 }
