b8dcc4e0c70c9e3a6ee7e9df464cd654d13c0f45
bjy <bjy@contek.io>
2021-12-24 14:39:54 +0800

    2.16.9-SNAPSHOT

--- bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/common/_UserTrade.java
+++ bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/common/_UserTrade.java
@@ -5,25 +5,22 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public class _UserTrade {
 
-  public Double closed_size;
-  public Long cross_seq;
-  public String exec_fee;
-  public String exec_id;
-  public String exec_price;
-  public Double exec_qty;
-  public String exec_type;
-  public String exec_value;
-  public String fee_rate;
-  public String last_liquidity_ind;
-  public Double leaves_qty;
-  public Integer nth_fill;
   public String order_id;
   public String order_link_id;
+  public String side;
+  public String symbol;
+  public String exec_id;
   public String order_price;
   public Double order_qty;
   public String order_type;
-  public String side;
-  public String symbol;
-  public Long user_id;
+  public String fee_rate;
+  public String exec_price;
+  public String exec_type;
+  public String exec_fee;
+  public String exec_value;
+  public Double exec_qty;
+  public Double leaves_qty;
+  public Double closed_size;
+  public String last_liquidity_ind;
   public Long trade_time_ms;
 }
