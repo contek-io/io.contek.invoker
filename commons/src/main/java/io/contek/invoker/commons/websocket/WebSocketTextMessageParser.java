fb89ec47f933e57ca8f30101af213c2a736e45ca
bjy <bjy@contek.io>
2021-12-27 12:08:37 +0800

    2.16.16-SNAPSHOT bybit websocket fix

--- commons/src/main/java/io/contek/invoker/commons/websocket/WebSocketTextMessageParser.java
+++ commons/src/main/java/io/contek/invoker/commons/websocket/WebSocketTextMessageParser.java
@@ -1,13 +1,25 @@
 package io.contek.invoker.commons.websocket;
 
+import org.slf4j.Logger;
+
 import javax.annotation.concurrent.ThreadSafe;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ThreadSafe
 public abstract class WebSocketTextMessageParser implements IWebSocketMessageParser {
 
+  private static final Logger log = getLogger(WebSocketTextMessageParser.class);
+
   @Override
   public final ParseResult parse(String text) {
-    return new ParseResult(text, fromText(text));
+    try {
+      AnyWebSocketMessage message = fromText(text);
+      return new ParseResult(text, message);
+    } catch (Throwable t) {
+      log.error("Failed to parse text message: {}.", text, t);
+      throw new WebSocketIllegalMessageException(t);
+    }
   }
 
   @Override
