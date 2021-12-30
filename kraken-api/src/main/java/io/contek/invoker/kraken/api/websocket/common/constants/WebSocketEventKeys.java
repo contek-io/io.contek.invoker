ad2b90781091e0955b12dca735446d7dc152aa0c
bjy <bjy@contek.io>
2021-12-30 18:19:57 +0800

    2.16.20-SNAPSHOT kraken keep alive

--- kraken-api/src/main/java/io/contek/invoker/kraken/api/websocket/common/constants/WebSocketEventKeys.java
+++ kraken-api/src/main/java/io/contek/invoker/kraken/api/websocket/common/constants/WebSocketEventKeys.java
@@ -5,9 +5,19 @@ import javax.annotation.concurrent.Immutable;
 @Immutable
 public final class WebSocketEventKeys {
 
-  public static final String _subscriptionStatus = "subscriptionStatus";
+  public static final String _ping = "ping";
+
+  public static final String _pong = "pong";
+
+  public static final String _heartbeat = "heartbeat";
 
   public static final String _systemStatus = "systemStatus";
+
+  public static final String _subscribe = "subscribe";
+
+  public static final String _unsubscribe = "unsubscribe";
+
+  public static final String _subscriptionStatus = "subscriptionStatus";
 
   private WebSocketEventKeys() {}
 }
