e5b77fd8e07f968f66d4a675d98ca3ffd6b4979b
bjy <bjy@contek.io>
2022-01-20 15:39:24 +0800

    minor change

---
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/websocket/market/constants/BookChangeActionKeys.java
@@ -0,0 +1,15 @@
+package io.contek.invoker.deribit.api.websocket.market.constants;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public final class BookChangeActionKeys {
+
+  public static final String _new = "new";
+
+  public static final String _change = "change";
+
+  public static final String _delete = "delete";
+
+  private BookChangeActionKeys() {}
+}
