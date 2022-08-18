621bdc38fd723b69d3acd18856ee3112953d15fb
bjy <bjy@contek.io>
2022-08-18 10:54:16 +0800

    2.20.8

--- bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/common/RestResponse.java
+++ bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/common/RestResponse.java
@@ -5,13 +5,13 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public abstract class RestResponse<R> {
 
-  public int ret_code;
+  public Integer ret_code;
   public String ret_msg;
   public String ext_code;
   public String ext_info;
-  public double time_now;
-  public int rate_limit_status;
-  public long rate_limit_reset_ms;
-  public int rate_limit;
+  public Double time_now;
+  public Integer rate_limit_status;
+  public Long rate_limit_reset_ms;
+  public Integer rate_limit;
   public R result;
 }
