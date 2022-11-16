c9efc0591bd9706daa91acfd70e7b7ab1006c25d
bjy <bjy@contek.io>
2022-11-16 18:00:45 +0800

    3.5.2

--- bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/common/RestError.java
+++ bybitlinear-api/src/main/java/io/contek/invoker/bybitlinear/api/rest/common/RestError.java
@@ -1,6 +1,8 @@
 package io.contek.invoker.bybitlinear.api.rest.common;
 
+import io.contek.invoker.commons.rest.Empty;
+
 import javax.annotation.concurrent.NotThreadSafe;
 
 @NotThreadSafe
-public final class RestError extends ResponseWrapper<EmptyResult> {}
+public final class RestError extends ResponseWrapper<Empty> {}
