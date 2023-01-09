6290e167512dcc3b4ffd973c718f215c812427a9
FLC <bjy.github@contek.io>
2023-01-09 13:22:14 +0800

    get leverage

--- okx-api/src/main/java/io/contek/invoker/okx/api/ApiFactory.java
+++ okx-api/src/main/java/io/contek/invoker/okx/api/ApiFactory.java
@@ -101,6 +101,7 @@ public final class ApiFactory {
         GetAccountAccountPositionRisk.RATE_LIMIT_RULE,
         GetAccountBalance.RATE_LIMIT_RULE,
         GetAccountConfig.RATE_LIMIT_RULE,
+        GetAccountLeverageInfo.RATE_LIMIT_RULE,
         GetAccountPositions.RATE_LIMIT_RULE,
         GetAssetBalances.RATE_LIMIT_RULE,
         GetTradeFills.RATE_LIMIT_RULE,
