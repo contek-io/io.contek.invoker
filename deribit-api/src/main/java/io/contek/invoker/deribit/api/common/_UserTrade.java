188f557bfd533c28d3d10852de81b116541bdb2d
FLC <bjy.github@contek.io>
2023-01-30 15:08:05 +0800

    3.6.12

--- deribit-api/src/main/java/io/contek/invoker/deribit/api/common/_UserTrade.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/common/_UserTrade.java
@@ -5,27 +5,33 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public class _UserTrade {
 
-  public Long amount;
-  public String block_trade_id;
-  public String direction;
-  public Double fee;
-  public String fee_currency;
-  public Double index_price;
+  public Long trade_seq;
+  public String trade_id;
+  public Long timestamp;
+  public Integer tick_direction;
+  public String state;
+  public Boolean self_trade;
+  public Boolean reduce_only;
+  public Double price;
+  public Boolean post_only;
+  public String order_type;
+  public String order_id;
+  public String matching_id;
+  public Double mark_price;
+  public String liquidity;
   public String instrument_name;
+  public Double index_price;
+  public String fee_currency;
+  public Double fee;
+  public String direction;
+  public Double amount;
+  public String advanced;
+  public String block_trade_id;
   public Double iv;
   public String label;
   public String liquidation;
-  public String liquidity;
-  public Double mark_price;
-  public String matching_id;
-  public String order_id;
-  public String order_type;
-  public String post_only;
-  public Double price;
   public Double profit_loss;
-  public Boolean reduce_only;
   public Double stop_price;
   public String trigger;
-  public String advanced;
   public Boolean mmp;
 }
--- deribit-api/src/main/java/io/contek/invoker/deribit/api/common/constants/LiquidityKeys.java
+++ deribit-api/src/main/java/io/contek/invoker/deribit/api/common/constants/LiquidityKeys.java
@@ -5,10 +5,9 @@ import javax.annotation.concurrent.Immutable;
 @Immutable
 public final class LiquidityKeys {
 
-  public static final String _taker = "taker";
+  public static final String _T = "T";
 
-  public static final String _maker = "maker";
+  public static final String _M = "M";
 
-  private LiquidityKeys() {
-  }
+  private LiquidityKeys() {}
 }
